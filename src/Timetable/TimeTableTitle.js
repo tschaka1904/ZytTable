ef7a9dccc225f30f2db20dfb0dea5ea46613f1c4
maximiliankoch <tschaka1904@gmail.com>
2019-06-12 21:26:10 +0200

    Basic TimeTable title. Ready to refactor.

---
+++ src/Timetable/TimeTableTitle.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {WHERE_AM_I} from "../Data/tram_info";
+
+class TimeTableTitle extends Component {
+    render() {
+        return (
+            <div className='row h-100'>
+                <div className="col-2 text-center">
+                </div>
+                <div className="col-8 text-center">
+                    <h1 style={{color: '#c7c3c3'}} className="pl-1"> {WHERE_AM_I.name} <span
+                        className="badge badge-danger">Live</span></h1>
+                </div>
+                <div className="col-2 text-center">
+                    <span className="float-right">Last update: {this.props.lastUpdate}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default TimeTableTitle;
