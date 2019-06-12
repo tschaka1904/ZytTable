109e9fd255015fa08413cb602e1a326f26d688a6
maximiliankoch <tschaka1904@gmail.com>
2019-06-12 21:47:13 +0200

    Updates

---
+++ precache-manifest.078ccad4ca4a4f764eff3052e9dbb736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd252650bb4a322eef6a01736a0f09fb",
+    "url": "/ZytTable/index.html"
+  },
+  {
+    "revision": "59459728ebc387081f74",
+    "url": "/ZytTable/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c3fbd79558cf14a8b582",
+    "url": "/ZytTable/static/css/main.7454695c.chunk.css"
+  },
+  {
+    "revision": "59459728ebc387081f74",
+    "url": "/ZytTable/static/js/2.df5fb876.chunk.js"
+  },
+  {
+    "revision": "c3fbd79558cf14a8b582",
+    "url": "/ZytTable/static/js/main.e907cdfe.chunk.js"
+  },
+  {
+    "revision": "c7e2c74ad0b4f89159b8",
+    "url": "/ZytTable/static/js/runtime~main.3f9ba573.js"
+  }
+]);
